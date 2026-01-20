fade941a1b25b040b2729f1ec1977e5790493ac8
kuusshagraa <kushagramisra06@gmail.com>
2026-01-21 01:18:52 +0530

    start pages

---
+++ reddit-clone/Components/Home.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+function Home() {
+  
+
+  return (
+    <>
+    <Link to='/LogIn'>LogIn</Link>
+
+    </>
+      
+  )
+}
+
+export default Home
